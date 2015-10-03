2fc33d10b26fbfd190b29e0ec6e5b21c74357c0b
Emil Persson <emil.n.persson@gmail.com>
2015-10-04 00:26:26 +0200

    Attempt to fix popState

--- lib/app_client/menu.dart
+++ lib/app_client/menu.dart
@@ -1,18 +1,31 @@
 part of app;
 
+final _menuItems = querySelectorAll('.menu-master a');
+
 void menu() {
   for (final toggler in querySelectorAll('.menu-toggle'))
     new Tapper(toggler).stream.listen(
         (_) => document.body.classes.toggle('menu-open'));
-  final menuItems = querySelectorAll('.menu-master a');
-  for (final item in menuItems)
+  for (final item in _menuItems)
     new Tapper(item).stream.listen((_) {
-      menuItems.forEach((e) => e.parent.classes.remove('active'));
+      _clearMenuActiveState();
       item.parent.classes.add('active');
       document.body.classes.remove('menu-open');
     });
-  menuItems
+  window.onPopState.listen((_) {
+    _clearMenuActiveState();
+    _setMenuActiveState();
+  });
+  _setMenuActiveState();
+}
+
+_setMenuActiveState() {
+  _menuItems
       .where(
       (AnchorElement e) => '${e.href}' == '${window.location}')
       .forEach((e) => e.parent.classes.add('active'));
+}
+
+_clearMenuActiveState() {
+  _menuItems.forEach((e) => e.parent.classes.remove('active'));
 }
