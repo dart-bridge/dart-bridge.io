3e8ad20ee782689c1d3263830115663776d72350
Emil Persson <emil.n.persson@gmail.com>
2015-10-03 17:13:58 +0200

    More highlights and viewport restrictions to fix menu bug

--- lib/app_client/menu.dart
+++ lib/app_client/menu.dart
@@ -4,7 +4,15 @@ void menu() {
   for (final toggler in querySelectorAll('.menu-toggle'))
     new Tapper(toggler).stream.listen(
         (_) => document.body.classes.toggle('menu-open'));
-  for (final item in querySelectorAll('.menu-master a'))
-    new Tapper(item).stream.listen(
-        (_) => document.body.classes.remove('menu-open'));
+  final menuItems = querySelectorAll('.menu-master a');
+  for (final item in menuItems)
+    new Tapper(item).stream.listen((_) {
+      menuItems.forEach((e) => e.parent.classes.remove('active'));
+      item.parent.classes.add('active');
+      document.body.classes.remove('menu-open');
+    });
+  menuItems
+      .where(
+      (AnchorElement e) => '${e.href}' == '${window.location}')
+      .forEach((e) => e.parent.classes.add('active'));
 }
