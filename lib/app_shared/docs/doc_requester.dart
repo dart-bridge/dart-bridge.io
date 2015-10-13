3e3fe59a4bb58b9704ce301f0d194cd10d1fd4c4
Emil Persson <emil.n.persson@gmail.com>
2015-10-13 16:36:44 +0200

    Highlights

--- lib/app_shared/docs/doc_requester.dart
+++ lib/app_shared/docs/doc_requester.dart
@@ -1,10 +1,16 @@
 part of app.shared;
 
 abstract class DocRequester {
+  static String host = 'dart-bridge.github.io';
+  static String prefix = 'docs/';
+  static bool https = true;
+
   Future<String> request(Uri url);
 
   Future<String> get(String path) {
     final cleanPath = path.replaceFirst(new RegExp('^/'), '');
-    return request(new Uri.https('dart-bridge.github.io', '/docs/$cleanPath.html'));
+    final route = '/$prefix$cleanPath.html';
+    final uri = https ? new Uri.https(host, route) : new Uri.http(host, route);
+    return request(uri);
   }
 }
--- lib/service_providers/docs_service_provider.dart
+++ lib/service_providers/docs_service_provider.dart
@@ -2,6 +2,11 @@ part of services;
 
 class DocsServiceProvider extends ServiceProvider {
   Future setUp(Application app) async {
+    if (Environment.isDevelopment) {
+      DocRequester.host = 'localhost:1337';
+      DocRequester.prefix = '';
+      DocRequester.https = false;
+    }
     app.bind(DocRequester, ServerDocRequester);
   }
 }
